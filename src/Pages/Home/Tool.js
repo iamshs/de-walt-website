79187016226e85b2653669b128761372a091a55c
iamshs <mdsolayman998@gmail.com>
2022-05-22 02:18:46 +0600

    tools added to homepgae

---
+++ src/Pages/Home/Tool.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Tool = ({tool}) => {
+    const {name,_id,description,img,price,minQuantity,availableQuantity} = tool
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl my-10">
+  <figure>
+      <img className='w-32 h-32 p-4' src={img} alt={name} />
+      </figure>
+  <div className="card-body">
+    <h2 className="card-title text-primary">{name}</h2>
+    <p className='font-semibold'>{description}</p>
+    <p className='text-xl '> <span className='font-bold'>Price:$</span>{price}</p>
+    <p className='text-lg'>Available Quantity: {availableQuantity}</p>
+    <p className=' text-lg font-bold'>Minimum Order Quantity: {minQuantity}</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-gradient-to-r from-cyan-500 to-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Tool;
---
+++ src/Pages/Home/Tools.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
+
+const Tools = () => {
+    const [tools,setTools]=useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/tool')
+    .then(res=>res.json())
+    .then(data=>{
+        
+        setTools(data)
+    })
+    },[])
+    return (
+        <div>
+          <h1 className='text-4xl font-bold my-10 text-center'>Tools we Provide</h1>
+          <div className=' grid grid-cols-1 lg:grid-cols-3 gap-4'>
+          {
+              tools.map(tool=><Tool key={tool._id} tool={tool} ></Tool> )
+          }    
+          </div>  
+        </div>
+    );
+};
+
+export default Tools;
