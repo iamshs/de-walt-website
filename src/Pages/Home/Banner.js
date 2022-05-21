da1c499d503e87c5597776b49f2a4332ea36c6fd
iamshs <mdsolayman998@gmail.com>
2022-05-21 23:05:13 +0600

    banner-added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,42 @@
+import React from "react";
+import banner1 from "../../assets/banner1.jpg";
+import banner2 from "../../assets/banner2.jpg";
+import banner3 from "../../assets/banner3.jpg";
+import banner4 from "../../assets/banner4.jpg";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src={banner1} className="w-full" alt="" />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img src={banner2} className="w-full" alt="" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src={banner3} className="w-full" alt="" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src={banner4} className="w-full" alt="" />
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
